d8a38f1c626e2eaebf20a73c3fb9c4fdcabdf622
roman <roman@ROMANPC>
2026-08-09 00:49:03 +0300

    TODO: Chat.

--- Client/main.cpp
+++ Client/main.cpp
@@ -62,9 +62,9 @@ void main()
 	if (iResult == SOCKET_ERROR)
 	{
 		DWORD dwError = WSAGetLastError();
-		
-		
-		cout << "Unable to connect to Server." <<endl;
+
+
+		cout << "Unable to connect to Server." << endl;
 		cout << FormatLastError(dwError, szError) << endl;
 		closesocket(connect_socket);
 		freeaddrinfo(result);
@@ -75,34 +75,37 @@ void main()
 	CHAR sendbuffer[BUFFER_LENGTH] = "Hello Server";
 	CHAR recvbuffer[BUFFER_LENGTH] = {};
 
-	iResult = send(connect_socket, sendbuffer, strlen(sendbuffer), 0);
-	if (iResult == SOCKET_ERROR)
-	{
-		cout << FormatLastError(WSAGetLastError(), szError) << endl;
-		cout << "Send failed:\t" << WSAGetLastError() << endl;
-		closesocket(connect_socket);
-		freeaddrinfo(result);
-		WSACleanup();
-		return;
-	}
-	cout << "Bytes send: " << iResult << endl;
-
 	do
 	{
-		iResult = recv(connect_socket, recvbuffer, BUFFER_LENGTH, 0);
-		if (iResult > 0)cout << recvbuffer << "(" << iResult << "Bytes)" << endl;
-		else if (result == 0) cout << "Connection closet" << endl;
-		else cout << FormatLastError(WSAGetLastError(), szError) << endl; //cout << "Reseive failed:\t" << WSAGetLastError() << endl;
-	} while (iResult > 0);
+		iResult = send(connect_socket, sendbuffer, strlen(sendbuffer), 0);
+		if (iResult == SOCKET_ERROR)
+		{
+			cout << FormatLastError(WSAGetLastError(), szError) << endl;
+			cout << "Send failed:\t" << WSAGetLastError() << endl;
+			closesocket(connect_socket);
+			freeaddrinfo(result);
+			WSACleanup();
+			return;
+		}
+		cout << "Bytes send: " << iResult << endl;
+
+		//do
+		//{
+			iResult = recv(connect_socket, recvbuffer, BUFFER_LENGTH, 0);
+			if (iResult > 0)cout << recvbuffer << "(" << iResult << "Bytes)" << endl;
+			else if (result == 0) cout << "Connection closet" << endl;
+			else cout << FormatLastError(WSAGetLastError(), szError) << endl; //cout << "Reseive failed:\t" << WSAGetLastError() << endl;
+		//} while (iResult > 0);
+		cin.getline(sendbuffer,BUFFER_LENGTH);
+	} while (strcmp(sendbuffer,"exit") != 0);
 
 	iResult = shutdown(connect_socket, SD_BOTH);
 	if (iResult == SOCKET_ERROR)
 	{
 		cout << FormatLastError(WSAGetLastError(), szError) << endl;
 		cout << "Shutdown failed: " << WSAGetLastError() << endl;
-		closesocket(connect_socket);
-		freeaddrinfo(result);
-		WSACleanup();
-		return;
 	}
+	closesocket(connect_socket);
+	freeaddrinfo(result);
+	WSACleanup();
 }
